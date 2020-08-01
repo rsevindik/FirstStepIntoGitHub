27016572332610ec86681d43ab0404d7eb38b4b7
rsevindik <rsuzem@gmail.com>
2020-08-01 09:25:33 -0400

    update HelloB20

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,6 +3,12 @@ public class HelloB20 {
 
         System.out.println("Let's do it.! ");
 
+        //git init  (use only when creating new project)
+        // git add . (use when adding a new class or version)
+        // git commit -m"add HelloB20.java"
+        //  (use "git add <file>..." to update what will be committed)
+        //  (use "git restore <file>..." to discard changes in working directory)
+        //(use "git add" and/or "git commit -a")
 
 
     }
